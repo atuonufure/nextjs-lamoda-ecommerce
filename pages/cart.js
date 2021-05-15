e867191c0d87cf9243fe2b7e4e9a14dfa2559394
atuonufure <atuonufure@gmail.com>
2021-05-16 00:51:56 +0300

    Updated cart

--- pages/cart.js
+++ pages/cart.js
@@ -4,23 +4,49 @@ import { useSelector } from "react-redux";
 
 export default function Cart() {
   const goods = useSelector((state) => state.cart.items);
-  console.log(goods);
+
+  const cartItems = [];
+  const duplicateItems = [];
+
+  for (let i = 0; i < goods.length; i++) {
+    cartItems.push(goods[i].payload);
+  }
+
+  for (let i = 0; i < cartItems.length; i++) {
+    if (!duplicateItems.includes(cartItems[i])) {
+      duplicateItems.push(cartItems[i]);
+    }
+  }
+
+  const filteredItems = duplicateItems.map((item) => [item, 0]);
+
+  for (let i = 0; i < cartItems.length; i++) {
+    filteredItems.forEach((item) => {
+      if (item[0].id === cartItems[i].id) {
+        item[1] += 1;
+      }
+    });
+  }
+
   return (
     <div className="flex flex-col items-center justify-center w-screen">
       <Header />
       <main className="flex flex-col justify-center w-full sm:w-9/12">
         <div className="m-4">{goods.length} items</div>
         <div>
-          {goods.map((item) => (
-            <div key={item.payload.id} className="flex items-center w-full sm:m-4 sm:p-4 border-2 sm:border-8">
+          {filteredItems.map((item) => (
+            <div
+              key={item[0].id}
+              className="flex items-center w-full sm:m-4 sm:p-4 border-2 sm:border-8"
+            >
               <div className="flex w-1/5 justify-center p-1">
-                <img src={item.payload.img} alt="" />
+                <img src={item[0].img} alt="" />
               </div>
-              <div className="flex w-1/5 justify-center p-1">{item.payload.name}</div>
+              <div className="flex w-1/5 justify-center p-1">{item[0].name}</div>
               <div className="flex w-1/5 justify-center p-1">
-                <button>-</button> 1 <button>+</button>
+                <button>-</button> {item[1]} <button>+</button>
               </div>
-              <div className="flex w-1/5 justify-center p-1">{item.payload.price} ₽</div>
+              <div className="flex w-1/5 justify-center p-1">{item[0].price} ₽</div>
               <div className="flex w-1/5 justify-center p-1">Удалить</div>
             </div>
           ))}
